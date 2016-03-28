4e1a4ff9fa09eed5478046a62779563c1239f4ae
Ohad Feder <ohadfeder@gmail.com>
2016-03-28 14:40:12 +0300

    Added the first game class and gameSlide class

---
+++ DynamicEx/app/src/main/java/com/example/ohad/dynamicex/GameSlide.java
@@ -0,0 +1,8 @@
+package com.example.ohad.dynamicex;
+
+/**
+ * Created by Samuel on 28/03/2016.
+ */
+public class GameSlide extends Slide {
+
+}
--- DynamicEx/app/src/main/java/com/example/ohad/dynamicex/XmlParser.java
+++ DynamicEx/app/src/main/java/com/example/ohad/dynamicex/XmlParser.java
@@ -85,6 +85,7 @@ public class XmlParser {
 					}
 
 				}
+
 			}
 
 		} catch (Exception ex) {
