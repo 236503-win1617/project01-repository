1591b36fbbbadaa73c55e6fb8e461a78a7b5544d
EvgeniyHvostenko <evgenhvost@gmail.com>
2016-04-08 12:24:24 +0300

    Switched to class loader instead of files

--- DesktopApp/src/Resources/FileResources.java
+++ DesktopApp/src/Resources/FileResources.java
@@ -1,18 +1,25 @@
 package Resources;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by Evgeniy on 3/19/2016.
  */
 public class FileResources {
-    private static final String NO_PICTURE_FILE_NAME = "desktopApp/resources/no_picture.jpg";
-    private static final String NO_VIDEO_FILE_NAME = "desktopApp/resources/no_video.jpg";
-    private static final String OK_FILE_NAME = "desktopApp/resources/ok.jpg";
+    private static final String NO_PICTURE_FILE_NAME = "no_picture.jpg";
+    private static final String NO_VIDEO_FILE_NAME = "no_video.jpg";
+    private static final String OK_FILE_NAME = "ok.jpg";
 
-    //TODO: maybe switch to resource loader
+    //TODO: switch to use input stream instead of file
     private static File getFileSource(String filename) {
-        return new File(filename);
+        try {
+            URI uri = FileResources.class.getResource(filename).toURI();
+            return new File(uri);
+        } catch (URISyntaxException e) {
+            return null;
+        }
     }
 
     public static final File noPictureAvailable = getFileSource(NO_PICTURE_FILE_NAME);
