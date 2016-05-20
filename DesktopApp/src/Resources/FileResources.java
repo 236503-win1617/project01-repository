91832f2587edf20ee9d5dcab5ef899aafd0b8beb
Evgeniy <evgenhvost@gmail.com>
2016-05-20 16:30:54 +0300

    Resources for pictures on slide buttons

--- DesktopApp/src/Resources/FileResources.java
+++ DesktopApp/src/Resources/FileResources.java
@@ -9,6 +9,9 @@ public class FileResources {
     private static final String NO_PICTURE_FILE_NAME = "no_picture.jpg";
     private static final String NO_VIDEO_FILE_NAME = "no_video.jpg";
     private static final String OK_FILE_NAME = "ok.jpg";
+    private static final String NO_PICTURE_BUTTON_FIL_NAME = "no-picture-button.jpg";
+    private static final String NO_VIDEO_BUTTON_FILE_NAME = "no-video-button.jpg";
+    private static final String VIDEO_SELECTED_BUTTON_FILE_NAME = "video-selected-button.jpg";
 
     private static InputStream getFileSource(String filename) {
         return FileResources.class.getResourceAsStream(filename);
@@ -24,5 +27,17 @@ public class FileResources {
 
     public static InputStream getOkStream() {
         return getFileSource(OK_FILE_NAME);
+    }
+
+    public static InputStream getButtonNoPicture() {
+        return getFileSource(NO_PICTURE_BUTTON_FIL_NAME);
+    }
+
+    public static InputStream getButtonNoVideo() {
+        return getFileSource(NO_VIDEO_BUTTON_FILE_NAME);
+    }
+
+    public static InputStream getVideoSelectedButton() {
+        return getFileSource(VIDEO_SELECTED_BUTTON_FILE_NAME);
     }
 }
